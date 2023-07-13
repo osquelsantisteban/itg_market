c93e43314f669672fc297fbc08baa2f69ae02d7e
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2023-07-12 20:48:09 -0400

    upd validate buyForm

--- src/composables/validation.js
+++ src/composables/validation.js
@@ -4,6 +4,18 @@ export default function useFormValidation() {
     return pattern.test(value);
   };
 
+  const validateOnlyInteger = (value) => {
+    let pattern = /^[0-9\s]+$/;
+    return pattern.test(value);
+  };
+
+  const validatePhone = (value) => {
+      let pattern1 = /^\+?[1-9]\d{1,14}$/;
+      let pattern2 = /^\+\d{1,2}\s?\(\d{3}\)\s?\d{3}-\d{4}$/;    
+    return pattern1.test(value) || pattern2.test(value);
+  };
+
+
   const validateEmail = (value) => {
     let pattern = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     return pattern.test(value);
@@ -11,6 +23,8 @@ export default function useFormValidation() {
 
   return {
     validateOnlyText,
+    validateOnlyInteger,
+    validatePhone,
     validateEmail,
   };
 }
--- src/views/BuyFormView.vue
+++ src/views/BuyFormView.vue
@@ -3,41 +3,62 @@
         <!-- Formulario -->
         <section class="flex gap-5">
             <div class="flex flex-col w-10/12 gap-5">
+                
                 <!-- Comprador -->
                 <div class="flex flex-col gap-5 my-2">
+                
                     <div class="flex gap-5 w-full">
-
+                        <!-- Nombre del Comprador -->
                         <div class="flex flex-col flex-1">
                             <input type="text" placeholder="Nombre del comprador" 
                                 v-model="form.name_buy" 
                                 :disabled="form.isSamePerson" 
+                                @input="validateNameBuy"
                                 class="input_form"
-                                :class="{ 'disable': form.isSamePerson ,'input_form_error': validateNameBuy}"
+                                :class="{ 'disable': form.isSamePerson ,'input_form_error': errors.name_buy}"
                                 title="Nombre solo acepta letras y espacios en blanco">
                             <span class="span_error">{{ errors.name_buy }}</span>
                         </div>
                         
+                        <!-- Apellidos del Comprador -->
                         <div class="flex flex-col flex-1">
                             <input type="text" placeholder="Apellidos del comprador" 
                                 v-model="form.last_name_buy" 
                                 :disabled="form.isSamePerson" 
+                                @input="validateLastNameBuy"
                                 class="input_form"
-                                :class="{ 'disable': form.isSamePerson,'input_form_error': validateLastNameBuy }"
+                                :class="{ 'disable': form.isSamePerson,'input_form_error': errors.last_name_buy }"
                                 title="Apellidos solo acepta letras y espacios en blanco">
-                            <span class="span_error">{{ errors.last_name_buy }}</span>                            
+                            <span class="span_error">{{ errors.last_name_buy }}</span>
                         </div>
                         
                     </div>
+
                     <div class="flex gap-5 w-full">
-                        <input type="text" placeholder="Email del comprador" 
-                            v-model="form.email_buy" 
-                            :disabled="form.isSamePerson" 
-                            class="input_form"
-                            :class="{ 'disable': form.isSamePerson,'input_form_error': validateEmailBuy}">
-                        <input type="text" placeholder="Teléfono del comprador" v-model="form.phone_buy" class="input_form"
-                            :disabled="form.isSamePerson" :class="{ 'disable': form.isSamePerson }">
+                        <!-- Email del Comprador -->
+                        <div class="flex flex-col flex-1">
+                            <input type="email" placeholder="Email del comprador" 
+                                v-model="form.email_buy" 
+                                :disabled="form.isSamePerson"
+                                @input="validateEmailBuy"
+                                class="input_form"
+                                :class="{ 'disable': form.isSamePerson,'input_form_error': errors.email_buy}">
+                            <span class="span_error">{{ errors.email_buy }}</span>
+                        </div>
+                        <!-- Teléfono del Comprador -->
+                        <div class="flex flex-col flex-1">
+                            <input type="tel" placeholder="Teléfono del comprador" 
+                                v-model="form.phone_buy" 
+                                :disabled="form.isSamePerson"
+                                @input="validatePhoneBuy"
+                                class="input_form"
+                                :class="{ 'disable': form.isSamePerson,'input_form_error': errors.phone_buy }">
+                            <span class="span_error">{{ errors.phone_buy }}</span>
+                        </div>
                     </div>
+
                 </div>
+
                 <!-- Receptor -->
                 <div class="flex flex-col gap-3">
                     <div class="flex gap-2">
@@ -45,22 +66,74 @@
                         <label for="same_person" class="text-cyan-800">Es la misma persona</label>                        
                     </div>
                     <div class="flex flex-col gap-5">
+
                         <div class="flex gap-5 w-full">
-                            <input type="text" placeholder="Nombre del receptor" v-model="form.name_receiver" class="input_form" required>
-                            <input type="text" placeholder="Apellido del receptor" v-model="form.last_name_receiver" required
-                                class="input_form">                            
-                        </div>
-                        <input type="text" placeholder="Dirección del receptor" v-model="form.address_name_receiver" required
-                            class="input_form">
-                        <div class="flex gap-5 w-full">
-                            <input type="text" placeholder="Email del receptor" v-model="form.email_receiver" class="input_form" required>
-                            <input type="text" placeholder="Teléfono del receptor" v-model="form.phone_receiver" required
-                                class="input_form">                            
+                            <!-- Nombre Receptor -->
+                            <div class="flex flex-col flex-1">
+                                <input type="text" placeholder="Nombre del receptor" 
+                                    v-model="form.name_receiver"
+                                    @input="validateNameReceiver"
+                                    class="input_form" required
+                                    :class="{'input_form_error': errors.name_receiver}">
+                                <span class="span_error">{{ errors.name_receiver }}</span>
+                            </div>
+
+                            <!-- Apellidos Receptor -->
+                            <div class="flex flex-col flex-1">
+                                <input type="text" placeholder="Apellidos del receptor" 
+                                    v-model="form.last_name_receiver"
+                                    @input="validateLastNameReceiver"
+                                    class="input_form" required
+                                    :class="{'input_form_error': errors.last_name_receiver}">
+                                <span class="span_error">{{ errors.last_name_receiver }}</span>
+                            </div>
                         </div>
 
-                        <input type="text" placeholder="Código postal del receptor" v-model="form.postal_code_receiver" required
-                            class="input_form">
+                        <!-- Dirección Receptor -->
+                        <div class="flex flex-col flex-1">
+                            <input type="text" placeholder="Dirección del receptor" 
+                                v-model="form.address_receiver" required
+                                class="input_form"
+                                :class="{'input_form_error': errors.address_receiver}">
+                            <span class="span_error">{{ errors.address_receiver }}</span>
+                        </div>
+
+                        <div class="flex gap-5 w-full">
+
+                            <!-- Email Receptor -->
+                            <div class="flex flex-col flex-1">
+                                <input type="email" placeholder="Email del receptor" 
+                                    v-model="form.email_receiver"
+                                    @input="validateEmailReceiver"
+                                    class="input_form" required
+                                    :class="{'input_form_error': errors.email_receiver}">
+                                <span class="span_error">{{ errors.email_receiver }}</span>
+                            </div>
+
+                            <!-- Teléfono Receptor -->
+                            <div class="flex flex-col flex-1">
+                                <input type="tel" placeholder="Teléfono del receptor" 
+                                    v-model="form.phone_receiver" required
+                                    @input="validatePhoneReceiver"
+                                    class="input_form"
+                                    :class="{'input_form_error': errors.phone_receiver}">
+                                <span class="span_error">{{ errors.phone_receiver }}</span>
+                            </div>
+                        </div>
+                        
+                        <!-- Código Postal Receptor -->
+                        <div class="flex flex-col flex-1">
+                            <input type="text" placeholder="Código postal del receptor" v-model="form.postal_code_receiver" required
+                                class="input_form"
+                                @input="validatePostalCodeReceiver"
+                                :class="{'input_form_error': errors.postal_code_receiver}">
+                            <span class="span_error">{{ errors.postal_code_receiver }}</span>
+                        </div>
                     </div>
+                </div>
+
+                <div class="flex justify-end">
+                    <button type="submit" class="btn-send" @click="sendToBuy">Enviar</button>
                 </div>
             </div>
             <div class="w-2/12">
@@ -74,10 +147,10 @@
 
 import MainLayout from '@/layouts/MainLayout.vue'
 import PanelCart from '@/components/PanelCart.vue';
-import { ref,computed,/*watch ,defineExpose */ } from 'vue';
+import { ref,/*computed,watch,defineExpose */ } from 'vue';
 import useFormValidation  from '@/composables/validation'
 
-const { validateOnlyText, validateEmail } = useFormValidation()
+const { validateOnlyText,validateOnlyInteger,validatePhone,validateEmail } = useFormValidation()
 const errors = ref({})
 
 const form = ref({
@@ -90,7 +163,7 @@ const form = ref({
     isSamePerson: false,
     name_receiver: '',
     last_name_receiver: '',
-    address_name_receiver: '',
+    address_receiver: '',
     email_receiver: '',
     phone_receiver: '',
     postal_code_receiver: '',
@@ -98,12 +171,16 @@ const form = ref({
 
 const disableIsSamePerson = () => {
     if (!form.value.isSamePerson) {
-        form.value.name_buy = null
-        form.value.last_name_buy = null
-        form.value.email_buy = null
-        form.value.phone_buy = null
+        form.value.name_buy = ''
+        form.value.last_name_buy = ''
+        form.value.email_buy = ''
+        form.value.phone_buy = ''
     }
 }
+
+const getFields = () => {
+  return Object.keys(form.value);
+};
 
 const validateField = (fieldName, validator, errorMessage) => {
   if (!validator(form.value[fieldName]) && form.value[fieldName] !== '') {
@@ -114,10 +191,61 @@ const validateField = (fieldName, validator, errorMessage) => {
   return false
 }
 
-let validateNameBuy     = computed(() => validateField('name_buy', validateOnlyText, 'El nombre no es válido'));
-let validateLastNameBuy = computed(() => validateField('last_name_buy', validateOnlyText, 'El apellido no es válido'));
-let validateEmailBuy    = computed(() => validateField('email_buy', validateEmail, 'El email no es válido'));
+const validateRequired = (requiredList) => {
+    let validateRulesRequired = true
+    requiredList.forEach((el) => {
+        if(!form.value[el]) {
+            errors.value[el] = 'El campo es requerido'
+            validateRulesRequired = false
+        }
+        else errors.value[el] = ''
+    })
 
+    return validateRulesRequired;  
+}
+
+// Validacion Comprador
+let validateNameBuy     = () => validateField('name_buy', validateOnlyText, 'El nombre no es válido');
+let validateLastNameBuy = () => validateField('last_name_buy', validateOnlyText, 'El apellido no es válido');
+let validateEmailBuy    = () => validateField('email_buy', validateEmail, 'El email no es válido');
+let validatePhoneBuy    = () => validateField('phone_buy', validatePhone, 'El teléfono no es válido');
+
+// Validacion Receptor
+let validateNameReceiver        = () => validateField('name_receiver', validateOnlyText, 'El nombre no es válido');
+let validateLastNameReceiver    = () => validateField('last_name_receiver', validateOnlyText, 'El apellido no es válido');
+let validateEmailReceiver       = () => validateField('email_receiver', validateEmail, 'El email no es válido');
+let validatePhoneReceiver       = () => validateField('phone_receiver', validatePhone, 'El teléfono no es válido');
+let validatePostalCodeReceiver  = () => validateField('postal_code_receiver', validateOnlyInteger, 'El código postal no es válido');
+
+const sendToBuy = (e) => {
+    e.preventDefault();
+
+    let requiredList = [];
+    if(form.value.isSamePerson)
+        // Solo estos son requeridos
+        requiredList = ['name_receiver','last_name_receiver','email_receiver','phone_receiver','address_receiver','postal_code_receiver']
+    else
+        // Todos son obligatorios
+        requiredList = getFields()
+    
+    // si true OK    
+    if(!validateRequired(requiredList)) return alert('Error de validación')
+
+    console.log(`Nombre Comprador ${form.value.name_buy}`)
+    console.log(`Nombre Receptor ${form.value.name_receiver}`)
+
+    console.log(`Apellidos Comprador ${form.value.last_name_buy}`)
+    console.log(`Apellidos Receptor ${form.value.last_name_receiver}`)
+
+    console.log(`Email Comprador ${form.value.email_buy}`)
+    console.log(`Email Receptor ${form.value.email_receiver}`)
+
+    console.log(`Phone Comprador ${form.value.phone_buy}`)
+    console.log(`Phone Receptor ${form.value.phone_receiver}`)
+
+    console.log(`Address Receptor ${form.value.address_receiver}`)
+    console.log(`Codigo Postal Receptor ${form.value.postal_code_receiver}`)
+}
 
 </script>
 
@@ -137,7 +265,7 @@ let validateEmailBuy    = computed(() => validateField('email_buy', validateEmai
     focus:border-transparent focus:ring-1 focus:ring-red-500 focus:border-red-500 focus:outline-none 
     focus:invalid:border-red-500 focus:invalid:ring-red-500
     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-    placeholder-slate-400
+    placeholder-red-500
     invalid:border-red-500 invalid:text-red-500;
 }
 
@@ -147,5 +275,9 @@ let validateEmailBuy    = computed(() => validateField('email_buy', validateEmai
 
 .span_error{
     @apply text-red-500 text-sm;
+}
+
+.btn-send{
+    @apply px-3 py-2 bg-amber-400 text-gray-800 rounded-lg w-44 hover:bg-amber-400/80
 }
 </style>
